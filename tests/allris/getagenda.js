95a405f6fd784613d178e096a7e8e80f3e86890b
Torsten Dreyer <torsten@t3r.de>
2019-09-22 19:47:24 +0200

    Add a getAgenda test, probably failing for now

--- tests/allris/getagenda.js
+++ tests/allris/getagenda.js
@@ -3,9 +3,9 @@ const expect = require('chai').expect;
 const assertArrays = require('chai-arrays');
 chai.use(assertArrays);
 
-function checkEntry( entry ) {
+function checkEntry( entry, length ) {
   expect(entry).to.be.array();
-  expect(entry.length).to.equal(24);
+  expect(entry.length).to.equal(length);
 
   expect(entry[0]).to.have.property('topic');
   expect(entry[0].topic).to.be.a('string');
@@ -19,9 +19,9 @@ module.exports = function() {
     const Allris = require('../../allris').Allris;
 
     const allris = new Allris('https://www.wentorf.sitzung-online.com/bi');
-    allris.getAgenda( { siLfdNr: 4210 } )
+    allris.getAgenda( { siLfdNr: 4198 } )
     .then( entry => {
-      checkEntry( entry );
+      checkEntry( entry, 16 );
       done();
     })
   });
@@ -30,9 +30,12 @@ module.exports = function() {
     const Allris = require('../../allris').Allris;
 
     const allris = new Allris('https://www.wentorf.sitzung-online.com/bi');
-    allris.getAgenda( { url: 'https://www.wentorf.sitzung-online.com/bi/to010.asp?SILFDNR=4210' } )
+    allris.getAgenda( { url: 'https://www.wentorf.sitzung-online.com/bi/to010.asp?SILFDNR=4198' } )
     .then( entry => {
-      checkEntry( entry );
+      checkEntry( entry, 16 );
+      entry.forEach( e => {
+        console.log( e.description.substring( 0, 40)  + (e.description.length > 40 ? '\u2026' : ''));
+      });
       done();
     })
   });
