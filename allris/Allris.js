f6d3afce58e96dd63917602ebdb2d0d4c6183090
Torsten Dreyer <torsten@t3r.de>
2019-09-25 08:42:10 +0200

    Add LICENSE and license headers

--- allris/Allris.js
+++ allris/Allris.js
@@ -1,8 +1,26 @@
 'use strict;';
+/*
+This file is part of twillris - a ALLRIS to Twitter bridge
+Copyright (C) 2019 Torsten Dreyer - torsten (at) t3r (dot) de
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 const got = require('got');
 const iCal = require('icalendar');
 const cheerio = require('cheerio');
-
+const moment = require('moment-timezone');
 
 const AllrisCalendarEntry = require('./AllrisCalendarEntry.js');
 const AllrisAgendaEntry = require('./AllrisAgendaEntry.js');
@@ -66,6 +84,62 @@ Allris.prototype.getAgenda = function( options ) {
     });
     return reply;
   })
+}
+
+Allris.prototype.ShrinkAgenda = function( agenda, maxLength ) {
+  const KillWords = /Einwohnerfragestunde|Niederschrift|Berichte|Anfragen|Anträge zur Tagesordnung|Eröffnung der Sitzung|Beschluss über den Ausschluss|Bekanntgabe der.*Beschlüsse|Bericht über die Ausführung/i
+
+  function AgendaLength( agenda ) {
+    let l = 0;
+    agenda.forEach( e => {
+      l += e.description.length
+    });
+    return l;
+  }
+
+  agenda = agenda.filter( entry => { 
+    return !KillWords.test( entry.description ); 
+  });
+
+  function ShrinkEntries( agenda, maxLength ) {
+    if( AgendaLength( agenda ) < maxLength ) return agenda;
+
+    let longest = 0;
+    agenda.forEach( e => {
+      longest = Math.max( e.description.length, longest );
+    });
+    agenda.forEach( e => {
+      if( e.description.length >= longest )
+        e.description = e.description.substring( 0, e.description.length-2 )+ '\u2026';
+    });
+    return ShrinkEntries( agenda, maxLength );
+  }
+
+  return ShrinkEntries( agenda, maxLength );
+}
+
+Allris.prototype.makeShortTweet = function( calendarEntry ) {
+  const TWEET_PREFIX = process.env.TWEET_PREFIX || 'Sitzung #Wentorf: ';
+  const status = `${TWEET_PREFIX}${moment(calendarEntry.startDate).tz('Europe/Berlin').format('D.M.YYYY HH:mm')} | ${calendarEntry.summary}, ${calendarEntry.location} (Tagesordnung: ${calendarEntry.link})`;
+  return status;
+}
+
+Allris.prototype.makeDetailedTweet = async function( calendarEntry ) {
+
+  const TWEET_PREFIX = process.env.TWEET_PREFIX || 'Sitzung #Wentorf: ';
+  let status = `${TWEET_PREFIX}${moment(calendarEntry.startDate).tz('Europe/Berlin').format('D.M.YYYY HH:mm')} | ${calendarEntry.summary} (`;
+
+  let agenda = await this.getAgenda( calendarEntry.link );
+
+  agenda = this.ShrinkAgenda( agenda, 278 - status.length - agenda.length*1 );
+  let topics = [""];
+  agenda.forEach( topic => {
+    topics.push( topic.description );
+  });
+
+  status += topics.join('\u2023') + ')';
+
+  return status;
 }
 
 module.exports = Allris;
--- tests/allris/tweets.js
+++ tests/allris/tweets.js
@@ -17,49 +17,39 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-
 const chai = require('chai');
 const expect = require('chai').expect;
 const assertArrays = require('chai-arrays');
 chai.use(assertArrays);
 
-function checkEntry( entry, length ) {
-  expect(entry).to.be.array();
-  expect(entry.length).to.equal(length);
-
-  expect(entry[0]).to.have.property('topic');
-  expect(entry[0].topic).to.be.a('string');
-
-  expect(entry[0]).to.have.property('description');
-  expect(entry[0].description).to.be.a('string');
-}
 
 module.exports = function() {
   const Allris = require('../../allris').Allris;
 
-  it('should fetch a calendar within a date range', async function() {
+  it('should create simple tweets for a day', async function() {
     const allris = new Allris('https://www.wentorf.sitzung-online.com/bi');
     let calendar = await allris.getCalendar( "30.09.2019" );
+    expect( calendar ).to.be.array();
+    calendar.forEach( entry => {
+      const tweet = allris.makeShortTweet( entry );
+      expect( tweet ).to.be.a('string');
+      console.log(tweet);
+    });
+
     return Promise.resolve();
   });
 
-  it('should fetch an agenda by siLfdNr', function(done) {
+  it('should create detailed tweets for a day', async function() {
     const allris = new Allris('https://www.wentorf.sitzung-online.com/bi');
-    allris.getAgenda( { siLfdNr: 4244 } )
-    .then( entry => {
-      checkEntry( entry, 15 );
-      done();
-    })
-  });
+    let calendar = await allris.getCalendar( "01.10.2019" );
+    expect( calendar ).to.be.array();
+    calendar.forEach( async(entry) => {
+      const tweet = await allris.makeDetailedTweet( entry );
+console.log(tweet.length, tweet);
+      expect( tweet ).to.be.a('string');
+      expect( tweet.length ).to.be.lt( 280 );
+    });
 
-  it('should fetch an agenda by url', function(done) {
-    const allris = new Allris('https://www.wentorf.sitzung-online.com/bi');
-    allris.getAgenda( { url: 'https://www.wentorf.sitzung-online.com/bi/to010.asp?SILFDNR=4198' } )
-    .then( agenda => {
-      checkEntry( agenda, 16 );
-      done();
-    })
+    return Promise.resolve();
   });
 }
-
-
